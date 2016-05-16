451d405f059cac75ec37d977539245fc87358a23
Huang Mao <351281668@qq.com>
2016-05-16 23:09:58 +0800

    创建app工程

---
+++ app/src/androidTest/java/com/dfy/hospital_app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dfy.hospital_app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
